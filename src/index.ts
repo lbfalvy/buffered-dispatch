3e3303c625e8f7b24fa25d1499a42fda6741d17c
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2023-02-08 18:39:19 +0000

    All tuples made readonly

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-export type Call<T extends any[], U> = {
+export type Call<T extends readonly any[], U> = {
     resolve: (u: U | PromiseLike<U>) => void
     reject: (e: any) => void
     args: T
 }
 
-export type CallOf<T extends (...args: any[]) => Promise<any>> =
+export type CallOf<T extends (...args: readonly any[]) => Promise<any>> =
     T extends (...args: infer Args) => Promise<infer Result>
         ? Call<Args, Result>
         : never
 
 export const constant = <T>(t: T) => ({ resolve }: Call<any[], T>) => resolve(t)
 export const thrower = (e: any) => ({ reject }: Call<any[], never>) => reject(e)
-export const wrap = <T extends any[], U>(
+export const wrap = <T extends readonly any[], U>(
     f: (...args: T) => U | Promise<U>
 ) => (
     { resolve, reject, args }: Call<T, U>
@@ -24,7 +24,7 @@ export const wrap = <T extends any[], U>(
     } catch(e) { reject(e) }
 }
 
-export function bufferedDispatch<T extends any[], U>(): [
+export function bufferedDispatch<T extends readonly any[], U>(): [
     (...args: T) => Promise<U>,
     () => Promise<Call<T, U>>
 ] {
